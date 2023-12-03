449fa5af1a12df6ee26a0e07650aabfd1bbf53bd
HarrisonGroom <89763078+HarrisonGroom@users.noreply.github.com>
2023-12-03 10:43:32 -0700

    ai backup to navmesh

--- GIMM-400-Unity/Assets/Scripts/AiRacer.cs
+++ GIMM-400-Unity/Assets/Scripts/AiRacer.cs
@@ -9,6 +9,8 @@ public class AiRacer : MonoBehaviour
     public float rotationSpeed = 2f;
 
     private int currentWaypointIndex = 0;
+    private int loopCounter = 0;
+    private int totalLoopsRequired = 3;
 
     void Update()
     {
@@ -25,6 +27,20 @@ public class AiRacer : MonoBehaviour
             if (Vector3.Distance(transform.position, waypoints[currentWaypointIndex].position) < 1f)
             {
                 currentWaypointIndex++;
+
+                // Check if the AI has completed a lap
+                if (currentWaypointIndex >= waypoints.Length)
+                {
+                    currentWaypointIndex = 0;
+                    loopCounter++;
+
+                    // Check if the AI has completed the required number of loops to win
+                    if (loopCounter >= totalLoopsRequired)
+                    {
+                        Debug.Log("AI has won!");
+                        // Implement the winning logic here, e.g., show a victory screen or end the race.
+                    }
+                }
             }
         }
     }
