c6d82e7aa9d32d5e112891e9e9e9b2a4a70383ce
Sean McKenzie <70048908+seanmckenzie428@users.noreply.github.com>
2024-04-11 16:19:04 -0600

    Created new trail shader with emission
    Duplicated the trail shader but added an emission color so that it would better match the player color

--- GIMM-400-Unity/Assets/Scripts/Racer.cs
+++ GIMM-400-Unity/Assets/Scripts/Racer.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Linq;
 using JetBrains.Annotations;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Racer : MonoBehaviour
@@ -11,6 +13,8 @@ public class Racer : MonoBehaviour
     public Rigidbody rb;
     public RaceManager raceManager;
     [CanBeNull] public Player player;
+    [SerializeField]
+    private TrailRenderer trailRenderer;
 
     private bool hitCheckpoint = false;
     private Transform lastCheckpoint;
@@ -22,6 +26,7 @@ public class Racer : MonoBehaviour
         rb = gameObject.GetComponent<Rigidbody>();
         rb.isKinematic = true;
         player = GetComponentInParent<Player>();
+        trailRenderer.emitting = false;
         // Invoke(nameof(SetSpawn), 0.2f);
     }
 
@@ -32,6 +37,20 @@ public class Racer : MonoBehaviour
             foreach (GameObject obj in coloredObjects)
             {
                 obj.GetComponent<Renderer>().material = playerMaterials[id - 1];
+            }
+
+            if (trailRenderer != null)
+            {
+                var mat = trailRenderer.material;
+                var playerMat = playerMaterials[id - 1];
+                var playerColor = playerMat.color;
+                var playerColorEmission = playerMat.GetColor("_EmissionColor");
+                // mat.shader.GetPropertyName(0);
+                // print(mat.shader.GetPropertyName(0));
+                mat.SetColor("_Color01", playerColor);
+                mat.SetColor("_Color02", playerColor);
+                mat.SetColor("_EmissionColor", playerColorEmission);
+                trailRenderer.material = mat;
             }
         }
     }
@@ -48,6 +67,14 @@ public class Racer : MonoBehaviour
         hitCheckpoint = false;
         lapsCompleted++;
         raceManager.CheckForWin();
+    }
+
+    public void EnableTrail()
+    {
+        if (trailRenderer != null)
+        {
+            trailRenderer.emitting = true;
+        }
     }
 
     public void OnTriggerEnter(Collider other)
