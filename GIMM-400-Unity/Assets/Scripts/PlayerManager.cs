d28fa6c69a54ceb9132bf1d65825ab7e35053d99
Sean McKenzie <70048908+seanmckenzie428@users.noreply.github.com>
2024-04-12 14:06:06 -0600

    Add 4 player models

--- GIMM-400-Unity/Assets/Scripts/PlayerManager.cs
+++ GIMM-400-Unity/Assets/Scripts/PlayerManager.cs
@@ -7,23 +7,30 @@ using UnityEngine.Serialization;
 
 public class PlayerManager : MonoBehaviour
 {
+    [SerializeField] private GameObject[] playerPrefabs;
     [SerializeField] private TextMeshProUGUI playerCountUI;
     private PlayerInputManager _playerInputManager;
+    private int playerPrefabIndex = 0;
 
     private void Start()
     {
         DontDestroyOnLoad(gameObject);
         _playerInputManager = GetComponent<PlayerInputManager>();
+        _playerInputManager.playerPrefab = playerPrefabs[playerPrefabIndex];
     }
 
     public void OnPlayerJoined(PlayerInput playerInput)
     {
         UpdatePlayerCountUI();
+        playerPrefabIndex++;
+        _playerInputManager.playerPrefab = playerPrefabs[playerPrefabIndex];
     }
 
     public void OnPlayerLeft(PlayerInput playerInput)
     {
         UpdatePlayerCountUI();
+        playerPrefabIndex--;
+        _playerInputManager.playerPrefab = playerPrefabs[playerPrefabIndex];
     }
 
     private void UpdatePlayerCountUI()
